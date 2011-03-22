77bbd46efaa70956e88449296ac41426f51f4e29
Ezequiel <exe.zaid@gmail.com>
2011-03-22 05:50:34 -0300

    add csrf protect

--- wiki/views.py
+++ wiki/views.py
@@ -1,6 +1,8 @@
 from wiki.models import Page
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_protect
+from django.template import RequestContext
 
 def view_page(request, page_name):
   try:
@@ -12,14 +14,17 @@ def view_page(request, page_name):
   return render_to_response("view.html", {"page_name":page_name, "content":content})
 
 
+@csrf_protect
 def edit_page(request, page_name):
   try:
     page = Page.objects.get(pk=page_name)
     content = page.content
   except Page.DoesNotExist:
     content= ""
-  return render_to_response('edit.html', {'page_name': page_name, 'content':content})
+  d = RequestContext(request,{'page_name':page_name, 'content':content})
+  return render_to_response('edit.html',d )
 
+@csrf_protect
 def save_page(request, page_name):
   content = request.POST['content']
   try:
@@ -28,4 +33,5 @@ def save_page(request, page_name):
   except Page.DoesNotExist:
     page = Page(name=page_name, content=content)
   page.save()
+  page = RequestContext(request, {'page': page})
   return HttpResponseRedirect("/wiki/" + page_name + "/")
