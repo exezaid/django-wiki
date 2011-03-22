547007760c7e4123d96b0756c4c3e794efc27eae
Ezequiel <exe.zaid@gmail.com>
2011-03-22 01:46:43 -0300

    fix

--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Page(models.Model):
-  name = models.CharFiedl(max_length="20", primary_key=True)
+  name = models.CharField(max_length="20", primary_key=True)
   content = models.TextField(blank=True)
