06dab4d568f9e9e9da362fa38cdd19b991a8edd0
Ezequiel <exe.zaid@gmail.com>
2011-03-22 02:47:55 -0300

    add edit

--- wiki/views.py
+++ wiki/views.py
@@ -7,3 +7,11 @@ def view_page(request, page_name):
     page = Page.objects.get(pk=page_name)
   except Page.DoesNotExist:
     return render_to_response('create.html', {'page_name': page_name})
+
+def edit_page(request, page_name):
+  try:
+    page = Page.objects.get(pk=page_name)
+    content = page.contents
+  except Page.DoesNotExist:
+    content= ""
+  return render_to_response('edit.html', {'page_name': page_name, 'content';conten})
