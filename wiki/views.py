e944c5d7134f55a79babbc98382a0133df49e16b
Ezequiel <exe.zaid@gmail.com>
2011-03-22 02:55:08 -0300

    edit view

--- wiki/views.py
+++ wiki/views.py
@@ -1,12 +1,16 @@
 from wiki.models import Page
 from django.shortcuts import render_to_response
-
+from django.http import HttpResponseRedirect
 
 def view_page(request, page_name):
   try:
     page = Page.objects.get(pk=page_name)
   except Page.DoesNotExist:
     return render_to_response('create.html', {'page_name': page_name})
+
+  content = page.content
+  return render_to_response("view.html", {"page_name":page_name, "content":content})
+
 
 def edit_page(request, page_name):
   try:
@@ -15,3 +19,13 @@ def edit_page(request, page_name):
   except Page.DoesNotExist:
     content= ""
   return render_to_response('edit.html', {'page_name': page_name, 'content';conten})
+
+def save_page(request, page_name):
+  content = request.POST['content']
+  try:
+    page = Page.objects.get(pk=page_name)
+    page.content = content
+  except Page.DoesNotExist:
+    page = Page(name=page_name, content=content)
+  page.save()
+  return HttpResponseRedirect("/wiki/" + page_name + "/")
